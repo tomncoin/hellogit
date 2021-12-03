52757f2df7c33006a641c092dbc5e86aebf8102d
blue <blue@DESKTOP-Q1MIF3C>
2021-12-03 15:01:34 +0700

    add cpm chalk

--- dog.js
+++ dog.js
@@ -1,3 +1,12 @@
-function Dog(){
-    this.stomach=[];
+var chalk = require("chalk");
+
+function Dog(name){
+    this.name=name;
 }
+
+Dog.prototype.sayHi = function(){
+    console.log("Hi! I am a dog. My name is " + chalk.blue(this.name));
+    //console.log("Hi! I am a dog. My name is " + this.name);
+}
+
+module.exports = Dog;
--- mouse.js
+++ mouse.js
@@ -1,3 +1,5 @@
 function Mouse(){
-    
+
 }
+
+module.exports = Mouse();
